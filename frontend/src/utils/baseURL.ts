1f2135b7e988f78732e6e4159704e6204f7ef44d
wahidbabar <bbabar0214@gmail.com>
2024-11-26 22:25:30 +0500

    updated config

--- frontend/src/utils/baseURL.ts
+++ frontend/src/utils/baseURL.ts
@@ -1,7 +1,7 @@
 // src/config/baseUrl.ts
 const getBaseUrl = () => {
   // Check if we're in a production environment
-  const isProduction = import.meta.env.NODE_ENV === "production";
+  const isProduction = import.meta.env.VITE_BACKEND_URL === "production";
 
   // Different base URLs for development and production
   if (isProduction) {
