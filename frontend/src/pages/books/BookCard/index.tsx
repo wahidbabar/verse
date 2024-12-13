b50062cc30751e295139168739b990a699d1957e
wahidbabar <bbabar0214@gmail.com>
2024-12-14 02:39:05 +0500

    Improved favorite book logic

--- frontend/src/pages/books/BookCard/index.tsx
+++ frontend/src/pages/books/BookCard/index.tsx
@@ -4,8 +4,6 @@ import { Card, CardContent } from "@/components/ui/card";
 import { BookImage } from "./BookImage";
 import { BookInfo } from "./BookInfo";
 import { BookActions } from "./BookAction";
-import { useToggleFavoriteBook } from "@/api/books";
-import { useState } from "react";
 
 interface BookCardProps {
   book: IBook;
@@ -14,20 +12,6 @@ interface BookCardProps {
 
 const BookCard = ({ book, layout = "grid" }: BookCardProps) => {
   const { addToCart, userId } = useCartStore();
-  const [isFavorite, setIsFavorite] = useState(
-    book.favoritedBy?.includes(userId!)
-  );
-  const toggleFavoriteBook = useToggleFavoriteBook(userId!);
-
-  const handleToggleFavorite = () => {
-    setIsFavorite(!isFavorite);
-
-    toggleFavoriteBook.mutate(book._id, {
-      onError: () => {
-        setIsFavorite((prev) => !prev);
-      },
-    });
-  };
 
   return (
     <Card className="h-full overflow-hidden hover:shadow-lg transition-shadow duration-300">
@@ -55,13 +39,7 @@ const BookCard = ({ book, layout = "grid" }: BookCardProps) => {
             author={book.author}
           />
 
-          <BookActions
-            book={book}
-            userId={userId}
-            onAddToCart={addToCart}
-            onToggleFavorite={handleToggleFavorite}
-            isFavorited={isFavorite}
-          />
+          <BookActions book={book} userId={userId} onAddToCart={addToCart} />
         </div>
       </CardContent>
     </Card>
